afed4eedb25e132679f21af1da894595db8f43c9
Mehedi <mirazmehedi065@gmail.com>
2025-07-30 12:41:29 +0600

    splash screen in progress

---
+++ lib/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:new_task/ui/screens/splash_screen.dart';
+
+class TaskManagerApp extends StatelessWidget {
+  const TaskManagerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
